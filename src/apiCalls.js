a05b45f048b323e0aa6e5194e3323c3a6699d583
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-15 08:50:46 -0700

    Add key id to urls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,17 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postURL = (long, newTitle) => {
+  return fetch(`http://localhost:3001/api/v1/urls`, {
+    method: 'POST',
+    body: JSON.stringify({
+      long_url: `${long}`,
+      title: `${newTitle}`
+      }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+}
